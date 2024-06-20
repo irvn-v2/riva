df2fc8bd54e56217f13a6066f3aa8739b471b04a
irvn-v2 <irvnmusili@gmail.com>
2024-06-20 06:55:33 +0300

    Update oop1.py

--- oop1.py
+++ oop1.py
@@ -4,8 +4,13 @@ class Student:
         self.course = course
         self.gender = gender
     def study(self):
-        print(self.firstname, "is studying")
+        print(self.firstname, "is a", self.gender, "and studies", self.course)
 
 student1 = Student("Sam", "Cybersecurity", "male")
 student1.study()
 
+student2 = Student("Irvn", "Python", "male")
+student2.study()
+
+student3 = Student("Nicole", "Software engineering", "female")
+student3.study()
